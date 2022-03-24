0ebb199c824fa20f9833f8d9d839cd6ea3712be0
Datsede04 <Datsede04@gmail.com>
2022-03-24 18:44:25 +0300

    limit updated to 8

--- src/config/config.js
+++ src/config/config.js
@@ -1,2 +1,2 @@
-export const POKEMON_API_URL ="https://pokeapi.co/api/v2/pokemon?limit=800"
+export const POKEMON_API_URL ="https://pokeapi.co/api/v2/pokemon?limit=8"
 export const IMAGE_API_URL = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"
--- src/containers/Pokedex.js
+++ src/containers/Pokedex.js
@@ -1,18 +1,37 @@
-import { Box } from '@material-ui/core'
+import { Box, CircularProgress } from '@material-ui/core'
 import axios from 'axios'
-import React, { useEffect } from 'react'
-import { POKEMON_API_URL } from '../config/config'
+import React, { useEffect, useState } from 'react'
+import { IMAGE_API_URL, POKEMON_API_URL } from '../config/config'
 
 function Pokedex() {
-  
+const [pokemonData,setPokemonData]= useState([])  
 useEffect(()=>{
     axios.get(POKEMON_API_URL).then((respones)=> {
-        console.log(respones)
+        console.log(respones)    
+        if(respones.status >= 200 && respones.status <=300){
+                const { results } =  respones.data;
+                let newPokemonData=[];
+                
+                results.forEach((pokemon, index) => {
+                    index++;
+                    let pokemonObject ={
+                        id: index,
+                        url: IMAGE_API_URL + index+".png",
+                        name: pokemon.name
+                    }
+                    newPokemonData.push(pokemonObject)
+
+                    console.log("PokemonObject", pokemonObject)
+                    console.log("pokemon", pokemon)
+                });
+                setPokemonData(newPokemonData)
+        }
     })
 },[])
 return (
-   <Box>Pokedex</Box>
-  
+   <Box>{pokemonData ? pokemonData.map((pokemon)=>{
+       return <h1>{pokemon.name}</h1>
+   }):<CircularProgress style={{marginTop:100}}/> }</Box>
   )
 }
 
